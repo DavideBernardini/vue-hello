4e371a0511fa5be36d7b1df1d8ecb35a3488639e
DavideBernardini <davidebernardini09@gmail.com>
2021-09-16 17:06:19 +0200

    completato esercizio

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+1. Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+2. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Hello Vue',
+            imgUrl: 'https://vueschool.io/img/svg/vueschool_hero.svg',
+            imgAlt: 'Vue factory',
+            imgClass: 'background',
+            titleClass: 'title',
+        }
+    }
+)
