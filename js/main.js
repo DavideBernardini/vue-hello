5aa7e767d2c24147824397eb4057a843e7a4903e
DavideBernardini <davidebernardini09@gmail.com>
2021-09-16 23:18:38 +0200

    modifica classi

--- js/main.js
+++ js/main.js
@@ -8,12 +8,9 @@ var app = new Vue(
         el: '#root',
         data: {
             message: 'Hello Vue',
-            imgUrl: 'https://vueschool.io/img/svg/vueschool_hero.svg',
+            imgUrls: ['https://vueschool.io/img/svg/vueschool_hero.svg', 'img/logo.svg'],
             imgAlt: 'Vue factory',
-            imgClass: 'background',
-            titleClass: 'title',
-            logoSrc: 'img/logo.svg',
-            logoClass: 'logo'
+            classes: ['background', 'title', 'logo']
         }
     }
 )
